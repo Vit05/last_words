55615a719f37f38bbeeff426a5f12eac931cb397
viktorpaliichuk <viktor.paliichuk@b2z-insurance.com>
2024-10-10 14:51:45 +0200

    update content 3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/last_words/',
   server: {
     host: '0.0.0.0',  // Разрешаем доступ со всех IP
     port: 3000,        // Порт по умолчанию (можете изменить его при необходимости)
